c1f509cdeee5c332461c2df71a36eea43aeae65e
KeJun <kejun1997@gmail.com>
2021-08-14 00:05:47 +0800

    feat(analyze): 使用 pandas 统计课程分数

---
+++ analyze.py
@@ -0,0 +1,32 @@
+from sqlalchemy import create_engine
+import os
+import json
+import pandas as pd
+from dotenv import load_dotenv
+from pandas.io.json import json_normalize
+
+load_dotenv()
+
+engine = create_engine(os.environ.get("DB_PATH"))
+
+df = pd.read_sql('course', engine)
+
+df["course_report"] = df["course_report"].apply(json.loads)
+
+
+for index, row in df.iterrows():
+    for item in row["course_report"]:
+        df.loc[index, item['名称']] = item['成绩']
+
+df = df.drop(["course_report", "semester"], axis=1)
+
+
+df = df.describe(exclude=[object])
+df = df.drop(["std", "25%", "50%", "75%"])
+
+df = df.T
+
+df.rename(columns={"count": "人数", "mean": "平均分",
+          'max': '最高分', 'min': "最低分"}, inplace=True)
+
+df.to_csv("analyze.csv")
